516ed0b80b56cd44c0c022f9bbd143bf0428f7fd
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-28 10:13:21 +0900

    Add Twoch#get

--- lib/twoch.rb
+++ lib/twoch.rb
@@ -2,4 +2,14 @@ require "twoch/version"
 
 class Twoch
   attr_accessor :url
+  attr_reader :body
+
+  def get
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host)
+    http.start do |http|
+      res = http.get(uri.path + '?' + uri.query)
+      @body = res.body
+    end
+  end
 end
--- spec/twoch_spec.rb
+++ spec/twoch_spec.rb
@@ -11,13 +11,29 @@ describe Twoch do
     end
   end
 
-  describe '#url=' do
-    let(:twoch) { Twoch.new }
+  let(:twoch) { Twoch.new }
 
+  describe '#url=' do
     context 'given a String' do
       it 'sets the url to url' do
         twoch.url = 'hogehoge url'
         expect(twoch.url).to eq('hogehoge url')
+      end
+    end
+  end
+
+  describe '#get' do
+    describe 'send a GET request' do
+      context 'when the responses 200' do
+        it 'response body is set to @body' do
+
+          WebMock.stub_request(:get, 'http://example.com/1?x=y')
+          .to_return(body: '<h1>body</h1>')
+
+          twoch.url = 'http://example.com/1?x=y'
+          twoch.get
+          expect(twoch.body).to eq('<h1>body</h1>')
+        end
       end
     end
   end
