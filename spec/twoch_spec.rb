8a042edb8a9dfe641e325ae41720033a0ee276ca
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-28 10:27:36 +0900

    Refactor

--- spec/twoch_spec.rb
+++ spec/twoch_spec.rb
@@ -24,23 +24,25 @@ describe Twoch do
 
   describe '#get' do
     describe 'send a GET request' do
-      context 'when the responses 200' do
-        it 'response body is set to @body' do
-          WebMock.stub_request(:get, 'http://example.com/1?x=y')
-          .to_return(body: '<h1>body</h1>')
+      before do
+        WebMock.stub_request(:get, 'http://example.com/1?x=y')
+        .to_return(body: '<h1>body</h1>', status: status)
+        twoch.url = 'http://example.com/1?x=y'
+      end
 
-          twoch.url = 'http://example.com/1?x=y'
+      context 'when the responses 200' do
+        let(:status) { 200 }
+
+        it 'response body is set to @body' do
           twoch.get
           expect(twoch.body).to eq('<h1>body</h1>')
         end
       end
 
       context 'when the responses 404' do
-        it 'raises' do
-          WebMock.stub_request(:get, 'http://example.com/1?x=y')
-          .to_return(body: '<h1>404</h1>', status: 404)
+        let(:status) { 404 }
 
-          twoch.url = 'http://example.com/1?x=y'
+        it 'raises' do
           expect { twoch.get }.to \
             raise_error(Twoch::HTTPError, 'Net::HTTPNotFound')
         end
