7a13fdea0351aa5a048b4e245f9ace48053a73f6
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 14:50:36 +0900

    Add Twoch::DatUri#convert

--- lib/twoch/dat_uri.rb
+++ lib/twoch/dat_uri.rb
@@ -2,7 +2,11 @@ class Twoch
   class DatUri < ::String
     # datのURLに変換する。
     def convert(arg)
-      arg
+      if %r(http://(\S+)\.2ch\.(\S+)/test/read.cgi/(\S+)/(\S+)/(\S*)) =~ arg
+        "http://#{$1}.2ch.#{$2}/#{$3}/dat/#{$4}.dat"
+      else
+        arg
+      end
     end
   end
 end
--- spec/twoch/dat_uri_spec.rb
+++ spec/twoch/dat_uri_spec.rb
@@ -1,8 +1,10 @@
 describe Twoch::DatUri do
   describe '#convert' do
     it do
-      actual = Twoch::DatUri.new.convert('')
-      expect(actual).to eq('')
+      input = 'http://anago.2ch.sc/test/read.cgi/geino/1370693658/'
+      output = Twoch::DatUri.new.convert(input)
+      expected = 'http://anago.2ch.sc/geino/dat/1370693658.dat'
+      expect(output).to eq(expected)
     end
   end
 end
