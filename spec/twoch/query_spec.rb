47635b015823f0111c2a31a9d507f0206a86cb00
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 13:21:58 +0900

    Refactor

--- spec/twoch/query_spec.rb
+++ spec/twoch/query_spec.rb
@@ -1,32 +1,31 @@
 describe Twoch::Query do
-  let(:query) { Twoch::Query.new }
-  it do
-    expect(query.query).to be query
-  end
+  let(:twoch) { double(:twoch) }
+  let(:query) { Twoch::Query.new(twoch) }
 
-  describe 'given query stuff' do
-    let(:twoch) { double(:twoch) }
-    let(:query) { Twoch::Query.new(twoch) }
-
-    it do
-      reses = []
-      expect(twoch).to receive(:reses).and_return(reses)
+  describe 'query has no twoch' do
+    let(:query) { Twoch::Query.new }
+    it 'returns self' do
       expect(query.query).to be query
     end
   end
 
-  describe 'given query stuff' do
-    let(:twoch) { double(:twoch) }
-    let(:query) { Twoch::Query.new(twoch) }
-
-    it do
-      hash = {from: 2}
-      res_1 = double(:res_1, res_index: 1)
-      res_2 = double(:res_2, res_index: 2)
-      res_3 = double(:res_3, res_index: 3)
-      reses = [res_1, res_2, res_3]
+  describe 'query has a twoch' do
+    it 'returns self' do
+      reses = []
       expect(twoch).to receive(:reses).and_return(reses)
-      expect(query.query(hash).result).to eq([res_2, res_3])
+      expect(query.query).to be query
+    end
+
+    context 'given a hash' do
+      it '#result returns the result' do
+        hash = {from: 2}
+        res_1 = double(:res_1, res_index: 1)
+        res_2 = double(:res_2, res_index: 2)
+        res_3 = double(:res_3, res_index: 3)
+        reses = [res_1, res_2, res_3]
+        expect(twoch).to receive(:reses).and_return(reses)
+        expect(query.query(hash).result).to eq([res_2, res_3])
+      end
     end
   end
 end
