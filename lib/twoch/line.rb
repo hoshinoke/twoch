349faf527e06139e07f4279fb2c989598c6f312d
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 10:06:39 +0900

    Add Line#user, #meta, #time, #body, #title

--- lib/twoch/line.rb
+++ lib/twoch/line.rb
@@ -1,4 +1,23 @@
 class Twoch
   class Line < ::String
+    def user
+      split('<>')[0]
+    end
+
+    def meta
+      split('<>')[1]
+    end
+
+    def time
+      split('<>')[2]
+    end
+
+    def body
+      split('<>')[3]
+    end
+
+    def title
+      split('<>')[4]
+    end
   end
 end
--- spec/twoch/line_spec.rb
+++ spec/twoch/line_spec.rb
@@ -2,4 +2,26 @@ describe Twoch::Line do
   it do
     expect(Twoch::Line.new).to be_a String
   end
+
+  let(:line) { Twoch::Line.new('user<>meta<>time<>body<>title') }
+
+  describe '#user' do
+    it { expect(line.user).to eq('user') }
+  end
+
+  describe '#meta' do
+    it { expect(line.meta).to eq('meta') }
+  end
+
+  describe '#time' do
+    it { expect(line.time).to eq('time') }
+  end
+
+  describe '#body' do
+    it { expect(line.body).to eq('body') }
+  end
+
+  describe '#title' do
+    it { expect(line.title).to eq('title') }
+  end
 end
