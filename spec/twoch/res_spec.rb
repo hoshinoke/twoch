6850d23b4d0d210c50c957fcd45d18b690fa7e8b
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 13:40:05 +0900

    Rename line -> res

--- spec/twoch/res_spec.rb
+++ spec/twoch/res_spec.rb
@@ -3,49 +3,49 @@ describe Twoch::Res do
     expect(Twoch::Res.new).to be_a String
   end
 
-  let(:line) { Twoch::Res.new('user<>meta<>time<>body<>title') }
-  let(:line_with_b_tag) do
+  let(:res) { Twoch::Res.new('user<>meta<>time<>body<>title') }
+  let(:res_with_b_tag) do
     Twoch::Res.new('user<b><>meta<b><>time<b><>body<b><>title<b>')
   end
 
   describe '#user' do
-    it { expect(line.user).to eq('user') }
+    it { expect(res.user).to eq('user') }
     it 'removes <b>' do
-      expect(line_with_b_tag.user).to eq('user')
+      expect(res_with_b_tag.user).to eq('user')
     end
   end
 
   describe '#meta' do
-    it { expect(line.meta).to eq('meta') }
+    it { expect(res.meta).to eq('meta') }
     it 'removes <b>' do
-      expect(line_with_b_tag.meta).to eq('meta')
+      expect(res_with_b_tag.meta).to eq('meta')
     end
   end
 
   describe '#time' do
-    it { expect(line.time).to eq('time') }
+    it { expect(res.time).to eq('time') }
     it 'removes <b>' do
-      expect(line_with_b_tag.time).to eq('time')
+      expect(res_with_b_tag.time).to eq('time')
     end
   end
 
   describe '#body' do
-    it { expect(line.body).to eq('body') }
-    it { expect(line.body).to be_a Twoch::Body }
+    it { expect(res.body).to eq('body') }
+    it { expect(res.body).to be_a Twoch::Body }
     it 'removes <b>' do
-      expect(line_with_b_tag.body).to eq('body')
+      expect(res_with_b_tag.body).to eq('body')
     end
   end
 
   describe '#title' do
-    it { expect(line.title).to eq('title') }
+    it { expect(res.title).to eq('title') }
     it 'removes <b>' do
-      expect(line_with_b_tag.title).to eq('title')
+      expect(res_with_b_tag.title).to eq('title')
     end
   end
 
   describe '#res_index' do
-    let(:line) { Twoch::Res.new('', index: 2) }
-    it { expect(line.res_index).to be 2 }
+    let(:res) { Twoch::Res.new('', index: 2) }
+    it { expect(res.res_index).to be 2 }
   end
 end
