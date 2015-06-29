edf1147be64c2378cb738bc2345d711cb3c7cfe1
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 13:29:44 +0900

    Refactor

--- spec/twoch/query_spec.rb
+++ spec/twoch/query_spec.rb
@@ -17,26 +17,21 @@ describe Twoch::Query do
     end
 
     context 'given a hash' do
+      let(:res_1){ double(:res_1, res_index: 1) }
+      let(:res_2){ double(:res_2, res_index: 2) }
+      let(:res_3){ double(:res_3, res_index: 3) }
+      let(:res_4){ double(:res_4, res_index: 4) }
+      let(:res_5){ double(:res_5, res_index: 5) }
+      let(:reses){ [res_1, res_2, res_3, res_4, res_5] }
+
       it '#result returns the result' do
         hash = {from: 2}
-        res_1 = double(:res_1, res_index: 1)
-        res_2 = double(:res_2, res_index: 2)
-        res_3 = double(:res_3, res_index: 3)
-        res_4 = double(:res_4, res_index: 4)
-        res_5 = double(:res_5, res_index: 5)
-        reses = [res_1, res_2, res_3, res_4, res_5]
         expect(twoch).to receive(:reses).and_return(reses)
         expect(query.query(hash).result).to eq([res_2, res_3, res_4, res_5])
       end
 
       it '#result returns the result' do
         hash = {from: 2, to: 4}
-        res_1 = double(:res_1, res_index: 1)
-        res_2 = double(:res_2, res_index: 2)
-        res_3 = double(:res_3, res_index: 3)
-        res_4 = double(:res_4, res_index: 4)
-        res_5 = double(:res_5, res_index: 5)
-        reses = [res_1, res_2, res_3, res_4, res_5]
         expect(twoch).to receive(:reses).and_return(reses)
         expect(query.query(hash).result).to eq([res_2, res_3, res_4])
       end
