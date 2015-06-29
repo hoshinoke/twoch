78c47cc855280e9496aeedab888234ee412bd1d4
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 14:35:03 +0900

    Add Twoch#scan

--- lib/twoch.rb
+++ lib/twoch.rb
@@ -2,17 +2,24 @@ require "twoch/body"
 require "twoch/query"
 require "twoch/res"
 require "twoch/version"
+require "pry"
 
 class Twoch
   attr_accessor *%i(body reses url)
 
   class HTTPError < StandardError; end
 
+  def scan(url)
+    self.url = url
+    get
+    parse_reses
+  end
+
   def get
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host)
     http.start do |http|
-      res = http.get(uri.path + '?' + uri.query)
+      res = http.get(uri.path + '?' + uri.query.to_s)
       @body = res.body
       if res.code.to_i >= 400
         raise HTTPError, res.class.to_s
