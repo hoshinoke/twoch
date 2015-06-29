b1624eb9a2dcaa12fbd6efeb564218dea8e21c6e
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 13:01:13 +0900

    Add Query#twoch

--- lib/twoch/query.rb
+++ lib/twoch/query.rb
@@ -1,6 +1,13 @@
 class Twoch
   class Query
+    def initialize(twoch = nil)
+      @twoch = twoch
+    end
+
+    attr_reader :twoch
+
     def query
+      twoch && twoch.reses
       self
     end
   end
--- spec/twoch/query_spec.rb
+++ spec/twoch/query_spec.rb
@@ -3,4 +3,15 @@ describe Twoch::Query do
   it do
     expect(query.query).to be query
   end
+
+  describe 'given query stuff' do
+    let(:twoch) { double(:twoch) }
+    let(:query) { Twoch::Query.new(twoch) }
+
+    it do
+      reses = []
+      expect(twoch).to receive(:reses).and_return(reses)
+      expect(query.query).to be query
+    end
+  end
 end
