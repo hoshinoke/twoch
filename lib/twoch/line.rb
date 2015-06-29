1c22bd588e703e66a65c95a94727ee5a896b218b
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-29 10:16:48 +0900

    Remove /<\/*b>/

--- lib/twoch/line.rb
+++ lib/twoch/line.rb
@@ -2,7 +2,7 @@ class Twoch
   class Line < ::String
     def initialize(*args)
       super
-      @user, @meta, @time, @body, @title = split('<>')
+      @user, @meta, @time, @body, @title = split('<>').map{|e| e.gsub(/<\/*b>/){''} }
     end
 
     attr_reader *%i(user meta time body title)
--- spec/twoch/line_spec.rb
+++ spec/twoch/line_spec.rb
@@ -4,24 +4,42 @@ describe Twoch::Line do
   end
 
   let(:line) { Twoch::Line.new('user<>meta<>time<>body<>title') }
+  let(:line_with_b_tag) do
+    Twoch::Line.new('user<b><>meta<b><>time<b><>body<b><>title<b>')
+  end
 
   describe '#user' do
     it { expect(line.user).to eq('user') }
+    it 'removes <b>' do
+      expect(line_with_b_tag.user).to eq('user')
+    end
   end
 
   describe '#meta' do
     it { expect(line.meta).to eq('meta') }
+    it 'removes <b>' do
+      expect(line_with_b_tag.meta).to eq('meta')
+    end
   end
 
   describe '#time' do
     it { expect(line.time).to eq('time') }
+    it 'removes <b>' do
+      expect(line_with_b_tag.time).to eq('time')
+    end
   end
 
   describe '#body' do
     it { expect(line.body).to eq('body') }
+    it 'removes <b>' do
+      expect(line_with_b_tag.body).to eq('body')
+    end
   end
 
   describe '#title' do
     it { expect(line.title).to eq('title') }
+    it 'removes <b>' do
+      expect(line_with_b_tag.title).to eq('title')
+    end
   end
 end
