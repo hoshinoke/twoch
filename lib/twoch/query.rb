7c16705436916bbd5c50b10aba9acf32c24d5151
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-07-04 14:12:47 +0900

    Refactor

--- lib/twoch/query.rb
+++ lib/twoch/query.rb
@@ -13,8 +13,8 @@ class Twoch
 
       result = result.select do |res|
         if hash[:refering_referred]
-          [twoch.ref_table.values].flatten.include?(res.res_index) \
-            || twoch.ref_table.key?(res.res_index)
+          [twoch.ref_table.values].flatten.include?(res.res_index) || \
+            twoch.ref_table.key?(res.res_index)
         elsif hash[:referred]
           [twoch.ref_table.values].flatten.include?(res.res_index)
         else
@@ -23,9 +23,8 @@ class Twoch
       end
 
       result = result.select do |res|
-        (!hash[:from]      || hash[:from] <= res.res_index) \
-          && \
-          (!hash[:to]        || hash[:to] >= res.res_index) \
+        (!hash[:from]      || hash[:from] <= res.res_index) && \
+          (!hash[:to]        || hash[:to] >= res.res_index)
       end
 
       result = result.select do |res|
@@ -33,8 +32,7 @@ class Twoch
           next(res.image? || res.youtube?)
         end
 
-        (!hash[:image]     || res.image?) \
-          && \
+        (!hash[:image]     || res.image?) && \
           (!hash[:youtube]   || res.youtube?)
       end
 
