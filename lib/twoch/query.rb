613f2139eb4a42fb7a90686274b809603a83c5e3
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-07-04 14:11:04 +0900

    Refactor

--- lib/twoch/query.rb
+++ lib/twoch/query.rb
@@ -8,38 +8,37 @@ class Twoch
     attr_reader :result, :twoch
 
     def query(hash = {})
-      if twoch
-        result = twoch.reses
+      return self if !twoch
+      result = twoch.reses
 
-        result = result.select do |res|
-          if hash[:refering_referred]
-            [twoch.ref_table.values].flatten.include?(res.res_index) \
-              || twoch.ref_table.key?(res.res_index)
-          elsif hash[:referred]
-            [twoch.ref_table.values].flatten.include?(res.res_index)
-          else
-            true
-          end
+      result = result.select do |res|
+        if hash[:refering_referred]
+          [twoch.ref_table.values].flatten.include?(res.res_index) \
+            || twoch.ref_table.key?(res.res_index)
+        elsif hash[:referred]
+          [twoch.ref_table.values].flatten.include?(res.res_index)
+        else
+          true
         end
+      end
 
-        result = result.select do |res|
-          (!hash[:from]      || hash[:from] <= res.res_index) \
+      result = result.select do |res|
+        (!hash[:from]      || hash[:from] <= res.res_index) \
           && \
           (!hash[:to]        || hash[:to] >= res.res_index) \
+      end
+
+      result = result.select do |res|
+        if hash[:image] && hash[:youtube]
+          next(res.image? || res.youtube?)
         end
 
-        result = result.select do |res|
-          if hash[:image] && hash[:youtube]
-            next(res.image? || res.youtube?)
-          end
-
-          (!hash[:image]     || res.image?) \
+        (!hash[:image]     || res.image?) \
           && \
           (!hash[:youtube]   || res.youtube?)
-        end
-
-        self.result.replace(result)
       end
+
+      self.result.replace(result)
 
       self
     end
