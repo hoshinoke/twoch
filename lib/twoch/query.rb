42e4a3caf42f1bfb61700fb6888e967b9d94dd02
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-07-04 14:18:52 +0900

    Refactor

--- lib/twoch/query.rb
+++ lib/twoch/query.rb
@@ -11,7 +11,21 @@ class Twoch
       return self if !twoch
       result = twoch.reses
 
-      result = result.select do |res|
+      result = filter_refer(result, hash)
+
+      result = filter_from_to(result, hash)
+
+      result = filter_image_youtube(result, hash)
+
+      self.result.replace(result)
+
+      self
+    end
+
+    private
+
+    def filter_refer(result, hash)
+      result.select do |res|
         if hash[:refering_referred]
           [twoch.ref_table.values].flatten.include?(res.res_index) || \
             twoch.ref_table.key?(res.res_index)
@@ -21,12 +35,16 @@ class Twoch
           true
         end
       end
+    end
 
+    def filter_from_to(result, hash)
       result = result.select do |res|
         (!hash[:from]      || hash[:from] <= res.res_index) && \
           (!hash[:to]        || hash[:to] >= res.res_index)
       end
+    end
 
+    def filter_image_youtube(result, hash)
       result = result.select do |res|
         if hash[:image] && hash[:youtube]
           next(res.image? || res.youtube?)
@@ -35,10 +53,6 @@ class Twoch
         (!hash[:image]     || res.image?) && \
           (!hash[:youtube]   || res.youtube?)
       end
-
-      self.result.replace(result)
-
-      self
     end
   end
 end
